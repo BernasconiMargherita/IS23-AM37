9eb96c492060436bc1b4305a6f710be3f4f3737d
NicolaCozzolino <nicola.cozzolino@mail.polimi.it>
2023-03-03 12:19:27 +0100

    Prima modifica

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Ciao a tutti! ho fatto una modifica!" );
+        System.out.println( "Ciao a tutti! ho fatto una modifica" );
     }
 }
