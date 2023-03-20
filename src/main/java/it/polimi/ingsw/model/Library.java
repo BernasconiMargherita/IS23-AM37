410577899aabf974ad596b5d36f236e9c3cb966e
margherita <tittaberna@gmail.com>
2023-03-20 12:52:40 +0100

    commit Library

--- src/main/java/it/polimi/ingsw/model/Library.java
+++ src/main/java/it/polimi/ingsw/model/Library.java
@@ -35,27 +35,28 @@ public class Library {
             tileMatrix[1][col] = tile3;
         }
         if(tile3 !=0){
-            tileMatrix[2][col] = tile3;
+
         }
-    }
 
-    //metodo per verificare se la libreria è piena
-    public boolean isFull(){
-        int count =0;
-        for (int i= 0; i<5; i++){
-            for(int j=0; j<4; j++){
-                if (tileMatrix[i][j] != 0) {
-                    count++;
-                }
-                //se la libreria è piena
-                if (count == 30){
-                    isFull() = true;
-                    return true;
+        //metodo per verificare se la libreria è piena
+        public boolean isFull(){
+            int count =0;
+            for (int i= 0; i<5; i++){
+                for(int j=0; j<4; j++){
+                    if (tileMatrix[i][j] != 0) {
+                        count++;
+                    }
+                    //se la libreria è piena
+                    if (count == 30){
+                        isFull() = true;
+                        return true;
 
+                    }
                 }
             }
+            isFull() = false;
+            return false;
         }
-        isFull() = false;
-        return false;
     }
-}
+    tileMatrix[2][col] = tile3;
+        }
