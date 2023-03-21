d9b03bb238d4965c84a85d8fe39e908ca634bb5b
NicolaCozzolino <nicola.cozzolino@mail.polimi.it>
2023-03-21 13:50:14 +0100

    Remade constructor method in CommonDeck in a more efficient way

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -4,7 +4,7 @@ public class Player {
     private String nickname;
 
     public Player(String nickname){
-        return this.nickname;
+        this.nickname=nickname;
     }
     public void addTilesinLibrary( Library library, int row, int column) {
         library.setValue(row, column, 1);
@@ -17,21 +17,5 @@ public class Player {
      * @return
      */
     public boolean checkCommonTarget( Library library, CardCommonTarget cardCommonTarget){
-        int matrix = cardCommonTarget.getMatrix();
-        int numRows = library.getNumRows();
-        int numCols = library.getNumCols();
-
-        for(int i=0; i< numRows; i++){
-            for(int j= 0; i< numCols; j++){
-                if(matrix[i][j] !=null){
-                    if(matrix[i][j] != library.getValue[i][j]){   /** qua adesso metto semplicemnte diverso **/
-                        return false;   /** in realtà poi controlliamo che il colore sia giusto etc **/
-                    }
-                    return true;
-
-                }
-            }
-        }
     }
-
 }
