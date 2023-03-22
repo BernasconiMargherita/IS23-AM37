f47908c9bdfedcf2de5dc66c9065b3f076b24e04
RamiroCicero <ramiro.cicero@mail.polimi.it>
2023-03-22 12:22:27 +0100

    commit :       -I modified PersonalCardTile (no more library but a vector with the information)       -I added the class PersonalCardTile ( pair of coordinates and colour)       -minor changes in utils

--- src/main/java/it/polimi/ingsw/model/CardPersonalTarget.java
+++ src/main/java/it/polimi/ingsw/model/CardPersonalTarget.java
@@ -1,37 +1,34 @@
 package it.polimi.ingsw.model;
 
-public class CardPersonalTarget extends Card {
-    /**
-     * TileSlot[][] personalMatrix : matrix (TileSlot) of the personalCard
-     */
-    private TileSlot[][] personalMatrix;
-    /**
-     *Utils utils : to fill the personalMatrix
-     */
-    Utils utils;
 
+
+public class CardPersonalTarget {
 
     /**
-     * costructor of CardPersonalTarget that assigns the personalType and fills the matrix
-     * @param personalType : identifies the goal of the personalCard
+     * vector of PersonalCardTile containing the information for assigning the personalCard
      */
-    public CardPersonalTarget(PersonalList personalType) {
-        utils = new Utils();
+    private final PersonalCardTile[] personalCardTiles;
 
 
-        personalMatrix = new TileSlot[6][5];
+    /**
+     * costructor : assignment of personal card
+     */
 
-
-
-        personalMatrix = utils.fillPersonalMatrix(personalMatrix, personalType);
+    public CardPersonalTarget(PersonalList personalType){
+        /**
+         *Utils utils : used for the assignment of the personal card
+         */
+        Utils utils = new Utils();
+        personalCardTiles = new PersonalCardTile[6];
+        utils.assignPersonalCard(personalCardTiles, personalType);
     }
 
-
     /**
-     * getter of the personalMatrix
-     * @return personalMatrix
+     *
+     * @return personalCardTiles vector containing the information of the personalCard
      */
-    public TileSlot[][] getPersonalMatrix() {
-        return personalMatrix;
+
+    public PersonalCardTile[] getPersonalCardTiles() {
+        return personalCardTiles;
     }
 }
