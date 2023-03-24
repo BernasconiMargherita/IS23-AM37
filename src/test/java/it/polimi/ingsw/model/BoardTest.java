1d0a36b423aa9715073b4ae43aa630d05c4e3926
margherita <tittaberna@gmail.com>
2023-03-24 13:47:10 +0100

    BoardTest

--- src/test/java/it/polimi/ingsw/model/BoardTest.java
+++ src/test/java/it/polimi/ingsw/model/BoardTest.java
@@ -6,10 +6,14 @@ import org.junit.jupiter.api.Test;
 /**
  * Unit test for simple App.
  */
+
 public class BoardTest {
     @Test
-    public void TestACaso(){
-        assertEquals(5,5);
+    public void twoPlayersCorrectTilesDisposition() {
+        Board board = new Board(2);
+        for(int i=0; i<8; i++ ){
+           assertTrue(board.getBoard()[0][i].isFree());
+        }
     }
 
 }
