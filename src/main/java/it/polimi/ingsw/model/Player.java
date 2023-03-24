34710bb2d8d6438bc537bc1ba8f02e87e3c104b8
NicolaCozzolino <nicola.cozzolino@mail.polimi.it>
2023-03-24 10:32:05 +0100

    minor changes

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -18,8 +18,27 @@ public class Player {
         this.utils=new Utils();
         this.score=0;
     }
+
+    public void setScore(int addedScore){
+        this.score += addedScore;
+    }
+
     public void addTilesInLibrary(int col,Tile[] selectedTile) throws NoSpaceInColumnException, FullColumnException {
         personalLibrary.addCardInColumn(col, selectedTile);
+    }
+
+    public void checkPersonalTarget(){
+        int checks=utils.checkPersonalTarget(personalLibrary,cardPersonalTarget);
+        switch (checks){
+            case 0: setScore(0);
+            case 1: setScore(1);
+            case 2: setScore(2);
+            case 3: setScore(3);
+            case 4: setScore(6);
+            case 5: setScore(9);
+            case 6: setScore(12);
+        }
+
     }
 
     public boolean checkCommonTarget(){
