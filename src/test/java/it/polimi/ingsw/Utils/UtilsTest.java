0a6b5658653288a097f0a3505fe41d48bb5e789c
margherita <tittaberna@gmail.com>
2023-04-04 15:17:28 +0200

    utils' test checkAllDifferent

--- src/test/java/it/polimi/ingsw/Utils/UtilsTest.java
+++ src/test/java/it/polimi/ingsw/Utils/UtilsTest.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.Utils;
 
 import it.polimi.ingsw.model.Tile.ColourTile;
-import org.junit.jupiter.api.Test;
+import it.polimi.ingsw.model.Tile.Tile;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 class UtilsTest {
 
     @Test
@@ -14,39 +16,50 @@ class UtilsTest {
     }
 
     @Test
-    int checkAllDifferent(TileSlot[] libraryMatrix, String type) {
-
-        TileSlot[] libraryMatrix = new TileSlot[];
-        assertEquals(5, checkAllDifferent(libraryMatrix, type));
-
-
-        if (type.equals("ROW")) {
-            libraryMatrix[0] = new TileSlot(ColourTile.CATS);
-            libraryMatrix[1] = new TileSlot(ColourTile.BOOKS);
-            libraryMatrix[2] = new TileSlot(ColourTile.GAMES);
-            libraryMatrix[3] = new TileSlot(ColourTile.FRAMES);
-            libraryMatrix[4] = new TileSlot(ColourTile.TROPHIES);
-            libraryMatrix[4] = new TileSlot(ColourTile.TROPHIES);
+    void checkAllDifferent() {
+        Utils utils=new Utils();
+        TileSlot[] libraryMatrix = new TileSlot[5];
+            for(int i=0; i<5;i++){
+                libraryMatrix[i] = new TileSlot();
+            }
 
 
+        libraryMatrix[0].assignTile(new Tile(ColourTile.CATS));
+        libraryMatrix[1].assignTile(new Tile(ColourTile.TROPHIES));
+        libraryMatrix[2].assignTile(new Tile(ColourTile.PLANTS));
+        libraryMatrix[3].assignTile(new Tile(ColourTile.BOOKS));
+        libraryMatrix[4].assignTile(new Tile(ColourTile.BOOKS));
 
+        assertEquals(4,utils.checkAllDifferent(libraryMatrix,"ROW"));
+
+
+
+        TileSlot[] libraryMatrix1 = new TileSlot[6];
+
+        for(int i=0; i<6;i++){
+            libraryMatrix1[i] = new TileSlot();
         }
 
-        if (type.equals("COLUMN")){
-            libraryMatrix[0] = new TileSlot(ColourTile.CATS);
-            libraryMatrix[1] = new TileSlot(ColourTile.BOOKS);
-            libraryMatrix[2] = new TileSlot(ColourTile.GAMES);
-            libraryMatrix[3] = new TileSlot(ColourTile.FRAMES);
-            libraryMatrix[4] = new TileSlot(ColourTile.TROPHIES);
 
+        libraryMatrix1[0].assignTile(new Tile(ColourTile.CATS));
+        libraryMatrix1[1].assignTile(new Tile(ColourTile.TROPHIES));
+        libraryMatrix1[2].assignTile(new Tile(ColourTile.PLANTS));
+        libraryMatrix1[3].assignTile(new Tile(ColourTile.BOOKS));
+        libraryMatrix1[4].assignTile(new Tile(ColourTile.BOOKS));
+        libraryMatrix1[5].assignTile(new Tile(ColourTile.BOOKS));
 
-
-        }
+        assertEquals(4,utils.checkAllDifferent(libraryMatrix1,"COLUMN"));
 
     }
 
     @Test
     void checkDiagonal() {
+        Utils utils=new Utils();
+        TileSlot[][] libraryMatrix = new TileSlot[6][5];
+
+        for(int i= 0; i< 6; i++){
+            for
+
     }
 
     @Test
