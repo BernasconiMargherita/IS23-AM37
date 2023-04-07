2be7c36462516138377d0bc509a30f91d654af7b
NicolaCozzolino <nicola.cozzolino@mail.polimi.it>
2023-04-07 16:12:48 +0200

    -code cleanup -small fixes

---
+++ src/test/java/it/polimi/ingsw/model/CommonCards/ScoringTokenTest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.model.CommonCards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoringTokenTest {
+
+    @Test
+    void getValueToken() {
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/Player/PlayerTest.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.model.Player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getNickname() {
+    }
+
+    @Test
+    void addScore() {
+    }
+
+    @Test
+    void addTilesInLibrary() {
+    }
+
+    @Test
+    void checkPersonalTarget() {
+    }
+
+    @Test
+    void setPersonalCard() {
+    }
+
+    @Test
+    void setFirstPlayer() {
+    }
+
+    @Test
+    void isFirstPlayer() {
+    }
+
+    @Test
+    void isShelfFull() {
+    }
+
+    @Test
+    void getPersonalShelf() {
+    }
+
+    @Test
+    void setCompleted() {
+    }
+
+    @Test
+    void isCompleted() {
+    }
+}
