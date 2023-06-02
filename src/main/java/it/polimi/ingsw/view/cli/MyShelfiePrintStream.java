69d9b9549b9ba93430633190d1c1eb5af8503d25
margherita <tittaberna@gmail.com>
2023-06-02 16:32:49 +0200

    Cli2

---
+++ src/main/java/it/polimi/ingsw/view/cli/MyShelfiePrintStream.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.view.cli;
+import java.io.PrintStream;
+public class MyShelfiePrintStream extends PrintStream {
+        MyShelfiePrintStream() {
+            super(System.out, true);
+        }
+    }
+}
